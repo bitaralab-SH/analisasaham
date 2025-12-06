aa83d74638c91c35a161a2cbca423eb2ad75e4c4
SaidiH <bitaralab@gmail.com>
2025-12-06 17:58:42 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig({
+  plugins: [react()],
+  base: '/analisasaham /' // 👈 ADD THIS!
+})
+
+/* export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
@@ -19,5 +24,5 @@ export default defineConfig(({ mode }) => {
           '@': path.resolve(__dirname, '.'),
         }
       }
-    };
-});
+    }; 
+}); */
